3e8c015c7252ae7ba8de606f1c896d612f00fdaf
pikesley <github@orgraphone.org>
2016-01-09 21:03:25 +0000

    Remove guff

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -6,13 +6,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'features/support/vcr'
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = false
-
-  [
-    'GITHUB_CLIENT_ID',
-    'GITHUB_CLIENT_SECRET'
-  ].each do |env_var|
-    c.filter_sensitive_data("<#{env_var}>") { ENV[env_var] }
-  end
 end
 
 VCR.cucumber_tags do |t|
--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -6,12 +6,5 @@ VCR.configure do |c|
   c.default_cassette_options = { :record => :once }
   c.allow_http_connections_when_no_cassette = false
 
-  [
-    'GITHUB_CLIENT_ID',
-    'GITHUB_CLIENT_SECRET'
-  ].each do |env_var|
-    c.filter_sensitive_data("<#{env_var}>") { ENV[env_var] }
-  end
-
   c.configure_rspec_metadata!
 end
