8f95c24b1f3bbe30f824bdcae05d80a3989c5758
pikesley <github@orgraphone.org>
2016-01-10 15:21:50 +0000

    This is suddenly fun

--- spec/javascripts/wtfSpec.js
+++ spec/javascripts/wtfSpec.js
@@ -4,11 +4,19 @@ describe('getRandom', function() {
   })
 })
 
-describe('isPlaceholder', function() {
-  it('recognises a non-placeholder', function() {
-    expect(isPlaceholder('nope')).toEqual(false)
+describe('Placeholders', function() {
+  describe('isPlaceholder', function() {
+    it('recognises a non-placeholder', function() {
+      expect(isPlaceholder('nope')).toEqual(false)
+    })
+    it('recognises a placeholder', function() {
+      expect(isPlaceholder('@yes')).toEqual(true)
+    })
   })
-  it('recognises a placeholder', function() {
-    expect(isPlaceholder('@yes')).toEqual(true) 
+
+  describe('stripFirst', function() {
+    it('removes the first character', function() {
+      expect(stripFirst('@thing')).toEqual('thing')
+    })
   })
 })
