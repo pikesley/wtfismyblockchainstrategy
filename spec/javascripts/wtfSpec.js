1276d0e822381a42eaeb1992fccfdbd552cc374f
pikesley <github@orgraphone.org>
2016-01-10 15:29:26 +0000

    More tests

--- spec/javascripts/wtfSpec.js
+++ spec/javascripts/wtfSpec.js
@@ -6,17 +6,26 @@ describe('getRandom', function() {
 
 describe('Placeholders', function() {
   describe('isPlaceholder', function() {
-    it('recognises a non-placeholder', function() {
-      expect(isPlaceholder('nope')).toEqual(false)
-    })
     it('recognises a placeholder', function() {
       expect(isPlaceholder('@yes')).toEqual(true)
+    })
+    it('recognises a non-placeholder', function() {
+      expect(isPlaceholder('nope')).toEqual(false)
     })
   })
 
   describe('stripFirst', function() {
     it('removes the first character', function() {
       expect(stripFirst('@thing')).toEqual('thing')
+    })
+  })
+
+  describe('containsPlaceholders', function() {
+    it('knows if a string contains any placeholders', function() {
+      expect(containsPlaceholders('this one @does contain placeholders')).toEqual(true)
+    })
+    it('knows if a string contains no placeholders', function() {
+      expect(containsPlaceholders('this is just a plain old string')).toEqual(false)
     })
   })
 })
